e9f8a528415dedc9a8e5cdfce1d04a929c67291c
SVasiliy <vasiliy.sharapov@gmail.com>
2018-06-08 10:13:57 -0500

    Exam topic: default methods

---
+++ src/enhancements/DefaultMethodsTopic.java
@@ -0,0 +1,48 @@
+package enhancements;
+
+// 1) Default methods can be overriden in implementing class, while static cannot.
+// 2) Static method belongs only to Interface class, so you can only invoke static method on Interface class, 
+//    not on class implementing this Interface.
+// 3) Both class and interface can have static methods with same names, and neither overrides the other.
+
+
+/* Example from Collections framework which explains the purpose of default methods.
+ * forEach was added and nothing was broken in the existing code
+ 
+    public interface Iterable<T> {
+       public void forEach(Consumer<? super T> consumer);
+    }
+*/
+
+
+interface MyInterface {
+	public void regularMethod();
+	
+	public default void defaultMethod() {            // doesn't have to be implemented
+		System.out.println("test default method");
+	};
+}
+
+class MyInterfaceClass implements MyInterface {
+	public void regularMethod() {
+		System.out.println("test regular method");
+	}
+}
+
+interface MyInterfaceWithDefault {
+	public void regularMethod();
+}
+
+public class DefaultMethodsTopic {
+
+	public static void main(String[] args) {
+		
+		System.out.println("test default methods");
+		
+		MyInterface test = new MyInterfaceClass();
+		test.regularMethod();
+		test.defaultMethod();
+		
+	}
+
+}
