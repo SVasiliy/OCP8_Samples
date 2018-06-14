ec5f2f0af3fc10186c8dc7ddb09d69e6b7925645
SVasiliy <vasiliy.sharapov@gmail.com>
2018-06-14 14:08:50 -0500

    Exam objective: streams
    flatMap() example

--- src/streams/StreamDemo.java
+++ src/streams/StreamDemo.java
@@ -1,5 +1,6 @@
 package streams;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -25,6 +26,16 @@ public class StreamDemo {
 		List<String> word2 = stream.collect(Collectors.toList());
 		System.out.println("word2: " + word2);
 		
+		
+		/////////////////////////////////////////////////////////////////
+		// flatMap() example
+		
+		List<String> l1 = Arrays.asList("a","b");
+		List<String> l2 = Arrays.asList("1","2");
+		
+		Stream.of(l1, l2).flatMap(x -> x.stream()).forEach(System.out::print);
+		System.out.println();
+		Stream.of(l1, l2).flatMap(x -> Stream.of(x)).forEach(System.out::print); // note the difference!
 		
 	}
 
