27264b9b255ced18135dbd774afd88ce8a224671
SVasiliy <vasiliy.sharapov@gmail.com>
2018-06-17 17:23:10 -0500

    Exam objective: streams
    Test non-final/non-effectively-final variable inside a lambda expression

--- src/streams/StreamAggregateDemo.java
+++ src/streams/StreamAggregateDemo.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class StreamAggregateDemo {
 
+
 	public static void main(String[] args) {
 
 		List<Double> list = Arrays.asList( 1.0, 2.0, 3.0, 4.0, 5.0);
@@ -12,6 +13,15 @@ public class StreamAggregateDemo {
 		double sum2 = list.stream().reduce(0.0, ( a, b) -> a + b);
 		
 		System.out.println("sum: " + sum + ", sum2: " + sum2);
+		
+		
+		// test non-final variable compilation error
+		/* uncomment to test
+		double sum3 = 0;
+		list.stream().forEach( x -> { sum3 = sum3 + x;});
+		System.out.println("sum3: " + sum3);
+		*/
+
 		
 	}
 
