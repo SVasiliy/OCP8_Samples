e7b2a8b8bf70ceeeb555f74b2f3bd1bb9090c06b
SVasiliy <vasiliy.sharapov@gmail.com>
2018-06-17 11:12:24 -0500

    Exam objective: streams
    Change fields in the TestObj class
    
    Change output loop to itereate over Map.Entry

--- src/streams/StreamGroupByDemo.java
+++ src/streams/StreamGroupByDemo.java
@@ -6,34 +6,52 @@ import java.util.stream.Collectors;
 enum E {A, B, C}
 
 class TestObj {
-	private E val;
-	public TestObj(E val) {
-		this.val = val;
+	
+	private E group;
+	private int value;
+	
+	public TestObj(E group, int value) {
+		this.group = group;
+		this.value = value;
 	}
-	public E getVal() {
-		return val;
+	
+	public E getGroup() {
+		return group;
+	}
+
+	public int getValue() {
+		return value;
 	}
 	
 	@Override
 	public String toString() {
-		return this.getVal().toString();
+		return this.getGroup().toString() + " - " + value;
 	}
 }
 
 public class StreamGroupByDemo {
 
-	public static void main(String[] args) {
+	public static void main( String[] args) {
 		
 		List<TestObj> tList = new ArrayList<>();
-		tList.add(new TestObj(E.A));
-		tList.add(new TestObj(E.A));
-		tList.add(new TestObj(E.B));
-		tList.add(new TestObj(E.C));
 		
-		Map<E,List<TestObj>> m = tList.stream().collect(Collectors.groupingBy(TestObj::getVal));
-		for (List<TestObj> s : m.values()) {
-			System.out.println("Next list: ");
-			for (TestObj o : s)
+		tList.add( new TestObj( E.A, 1));
+		tList.add( new TestObj( E.A, 2));
+		tList.add( new TestObj( E.B, 1));
+		tList.add( new TestObj( E.C, 1));
+		
+		Map<E, List<TestObj>> m = tList.stream()
+									  	.collect( 
+													Collectors.groupingBy( TestObj::getGroup)
+												);
+		
+		// outer loop
+		for (Map.Entry<E, List<TestObj>> s : m.entrySet()) {
+			
+			System.out.println("Next list (" + s.getKey() + "): ");
+			
+			// inner loop
+			for ( TestObj o : s.getValue())
 				System.out.println(o);
 		}
 	}
