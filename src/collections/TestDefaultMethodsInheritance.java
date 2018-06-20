b0fdddf7ce085505817f7d0323b03cb9fcfcda4e
SVasiliy <vasiliy.sharapov@gmail.com>
2018-06-19 21:50:55 -0500

    Exam objective: enhancements
    Test default methods inheritance

---
+++ src/collections/TestDefaultMethodsInheritance.java
@@ -0,0 +1,26 @@
+package collections;
+
+interface House {
+	public default String getAddress(){
+		return "101 Main Str";
+	} 
+}  
+
+interface Bungalow extends House {
+	public default String getAddress(){
+		return "101 Smart Str";
+	} 
+}  
+
+class MyHouse implements Bungalow, House {}  
+
+
+
+
+public class TestDefaultMethodsInheritance {
+
+	public static void main(String[] args) {
+		House ci = new MyHouse(); //1     
+		System.out.println(ci.getAddress()); // 101 Smart Str !!!   
+	} 
+}
