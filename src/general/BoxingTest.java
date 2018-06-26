09218be4ad9137a9ad82a1beb791498e21a83d69
SVasiliy <vasiliy.sharapov@gmail.com>
2018-06-26 09:31:10 -0500

    Exam objective: general
    Test boxing/unboxing

---
+++ src/general/BoxingTest.java
@@ -0,0 +1,19 @@
+package general;
+
+public class BoxingTest {
+
+	public static void main(String[] args) {
+
+		double d = new Integer(10);          // possible
+		
+		int i = 10;
+		double d2 = i;                       // possible
+		
+		Double d3 = d2;
+
+		//	Double d4 = i;                   // not possible
+		//	Double d5 = new Integer(10);     // not possible
+		
+	}
+
+}
