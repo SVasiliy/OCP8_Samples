4bef96473adf9455c581e8cc0859488bd9837959
SVasiliy <vasiliy.sharapov@gmail.com>
2018-06-29 14:32:19 -0500

    Exam objective: nio2
    Get root directories and file stores

--- src/nio2/SystemProviderDemo.java
+++ src/nio2/SystemProviderDemo.java
@@ -1,6 +1,11 @@
 package nio2;
 
+import java.nio.file.FileStore;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.nio.file.spi.FileSystemProvider;
+import java.util.Iterator;
 import java.util.List;
 
 public class SystemProviderDemo {
@@ -10,7 +15,20 @@ public class SystemProviderDemo {
         // list of available providers
 		// usually there are 2: default and zip/jar file system provider
 		List<FileSystemProvider> lst = FileSystemProvider.installedProviders();
+
+		// default file system
+		FileSystem defaultFS = FileSystems.getDefault();
 		
+		// root directories
+		Iterable<Path> roots = defaultFS.getRootDirectories();
+		for (Path s : roots)
+			System.out.println("root: " + s);  // root: C:\
+		
+		// file stores
+		Iterable<FileStore> stores = defaultFS.getFileStores();
+		Iterator iStores = stores.iterator();
+		FileStore fStore = (FileStore) iStores.next();
+		System.out.println( fStore.toString() + " " + fStore.name() + " " + fStore.type()); // OS (C:) OS NTFS
 	}
 
 }
