32be15bc6ad7564557e6ef7ad96ba1ac018e9fac
SVasiliy <vasiliy.sharapov@gmail.com>
2018-07-04 12:26:57 -0500

    Exam objective: nio.2
    Test Path components

---
+++ src/nio2/PathComponents.java
@@ -0,0 +1,32 @@
+package nio2;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathComponents {
+
+	public static void printPathInformation( Path path) {
+		
+		System.out.println( "Filename is: " + path.getFileName());
+		System.out.println( "Root is: " + path.getRoot());
+	
+		Path currentParent = path;
+		while((currentParent = currentParent.getParent()) != null) {
+			System.out.println("Current parent is: " + currentParent);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		printPathInformation( Paths.get("/zoo/armadillo/shells.txt"));  // filename: shells.txt
+																        // root: /
+		
+		
+		printPathInformation( Paths.get("/"));  // filename: null
+		                                        // parent: null
+		                                        // root: /
+		
+	}
+
+}
