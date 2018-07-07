37323e89207652ef15827b2f7301a298e09272af
SVasiliy <vasiliy.sharapov@gmail.com>
2018-07-07 17:20:19 -0500

    Exam objective: nio.2
    Test Files.readAllLines()

--- src/nio2/PathCopy.java
+++ src/nio2/PathCopy.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
 
 public class PathCopy {
 
@@ -13,6 +14,11 @@ public class PathCopy {
 			Files.copy( Paths.get("misc\\test_doc.txt"), 
 							Paths.get("misc\\test_doc_copy.txt"), 
 								StandardCopyOption.REPLACE_EXISTING);
+			
+			final List<String> lines = Files.readAllLines(Paths.get("misc\\test_doc_copy.txt"));
+			for (String line : lines) 
+				System.out.println(line);
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
