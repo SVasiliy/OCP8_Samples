8455c2021f10f93f3b6229c7dd2de41a34e0195a
SVasiliy <vasiliy.sharapov@gmail.com>
2018-07-07 17:40:20 -0500

    Exam objective: nio.2
    Test nio.2 views aka BasicFileAttributes

--- src/nio2/PathCopy.java
+++ src/nio2/PathCopy.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 
 public class PathCopy {
@@ -18,6 +19,14 @@ public class PathCopy {
 			final List<String> lines = Files.readAllLines(Paths.get("misc\\test_doc_copy.txt"));
 			for (String line : lines) 
 				System.out.println(line);
+			
+			BasicFileAttributes data = Files.readAttributes(
+												Paths.get("misc\\test_doc_copy.txt"), 
+													BasicFileAttributes.class);
+			
+			System.out.println("Is path a directory? " + data.isDirectory());
+			System.out.println("Is path a regular file? " + data.isRegularFile());
+			System.out.println("Last modified time: " + data.lastModifiedTime());
 			
 		} catch (IOException e) {
 			e.printStackTrace();
