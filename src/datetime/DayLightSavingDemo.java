1e7e95164faa20976eb7ede7e304c9b46ad9013e
SVasiliy <vasiliy.sharapov@gmail.com>
2018-08-13 11:47:41 -0500

    Exam objective: DateTime
    Testing DST

---
+++ src/datetime/DayLightSavingDemo.java
@@ -0,0 +1,29 @@
+package datetime;
+
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+
+public class DayLightSavingDemo {
+
+	public static void main(String[] args) {
+
+
+	LocalDateTime ld1 = LocalDateTime.of(2015, Month.NOVEMBER, 1, 2, 0);
+	ZonedDateTime zd1 = ZonedDateTime.of(ld1, ZoneId.of("US/Eastern"));
+	LocalDateTime ld2 = LocalDateTime.of(2015, Month.NOVEMBER, 1, 1, 0);
+	ZonedDateTime zd2 = ZonedDateTime.of(ld2, ZoneId.of("US/Eastern"));
+
+	long x = ChronoUnit.HOURS.between(zd1, zd2);
+	System.out.println(x);                                 // -2
+
+	LocalDateTime lod1 = LocalDateTime.of(2015, Month.MARCH, 8, 3, 0);    
+	ZonedDateTime zod1 = ZonedDateTime.of(lod1, ZoneId.of("US/Eastern"));  	
+	LocalDateTime lod2 = LocalDateTime.of(2015, Month.MARCH, 8, 2, 0);    
+	ZonedDateTime zod2 = ZonedDateTime.of(lod1, ZoneId.of("US/Eastern"));  	
+
+	long z = ChronoUnit.HOURS.between(zod1, zod2);
+	System.out.println(z);                                 // 0
+	
+	}
+
+}
