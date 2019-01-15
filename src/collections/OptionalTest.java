acd9c9d3d8067c06516e5e7d5666d729e33c1a4f
SVasiliy <vasiliy.sharapov@gmail.com>
2019-01-15 11:27:24 -0600

    Optional, OrElse, OrElseGet, OrElseThrow testing

---
+++ src/collections/OptionalTest.java
@@ -0,0 +1,33 @@
+package collections;
+
+import java.util.Optional;
+
+public class OptionalTest {
+	
+	public static Double getPrice(String id) {
+		
+		if (id == "1")
+			return 10.0;
+		else if (id == "2")
+			return 20.0;
+		else 
+			return null;
+		
+	}
+
+	public static void main(String[] args) {
+		
+		Optional<Double> price = Optional.ofNullable(getPrice("10"));
+		Double x = price.orElse(getPrice("2"));
+		
+		Double y = price.orElseGet(() -> new Double(20));
+
+		System.out.println("x: " + x);
+		System.out.println("y: " + y);
+		
+		Double z = price.orElseThrow(() -> new RuntimeException("Bad code!"));
+		
+	}
+
+}
+;
