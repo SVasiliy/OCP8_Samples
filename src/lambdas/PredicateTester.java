e7dfbef87b5c318cfcafb9241db21ce2c2f31ed9
SVasiliy <vasiliy.sharapov@gmail.com>
2019-01-19 10:22:57 -0600

    Test Predicate options

---
+++ src/lambdas/PredicateTester.java
@@ -0,0 +1,48 @@
+package lambdas;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import beans.Category;
+import beans.Product;
+import beans.ProductList;
+
+
+class ProductPredicate implements Predicate<Product> {
+	public boolean test(Product product) {
+		return product.getCategory() == Category.B;
+	}
+}
+
+class ProductPredicate2 {
+	public static boolean testProduct (Product product) {
+		return product.getCategory() == Category.B;
+	}
+}
+
+public class PredicateTester {
+	
+
+	public static void main(String[] args) {
+
+		
+		List<Product> lst = ProductList.getProductList();
+		
+		System.out.println(lst);
+		
+		// filter category B products
+		lst.stream().filter(new ProductPredicate()).forEach(System.out::println);
+		
+		// filter category B products (method 2)
+		lst.stream().filter(ProductPredicate2::testProduct).forEach(System.out::println);
+
+		// filter category B products (method 3)
+		lst.stream().filter(x -> ProductPredicate2.testProduct(x)).forEach(System.out::println);
+
+		// filter category B products (method 4)
+		lst.stream().filter(x -> new ProductPredicate().test(x)).forEach(System.out::println);
+		
+		
+	}
+
+}
