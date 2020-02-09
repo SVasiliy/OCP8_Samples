fa0f9c7cbea6dfaf96a29d8ee45887ddcc3606d3
SVasiliy <vasiliy.sharapov@gmail.com>
2020-02-09 11:32:37 -0600

    none

---
+++ src/test/java/misc/InheritanceTest.java
@@ -0,0 +1,68 @@
+package misc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class InheritanceTest {
+
+    static class X
+    {
+        char character;
+
+        X(char character) {
+            this.character = character;
+        }
+    }
+
+    static class Y extends X
+    {
+        char character = 'A';
+
+        Y() {
+            super('B');
+        }
+
+        public char getSuper(){ return super.character; }
+
+        class Z extends X
+        {
+            char character = 'C';
+
+            Z() {
+                super('D');
+            //    System.out.println(Y.Z.this.character);
+            //    System.out.println(super.character);
+            }
+
+            public char getA(){
+                return Y.this.character;
+            }
+            public char getB(){
+                return Y.this.getSuper();
+            }
+            public char getC(){
+                return Y.Z.this.character;
+            }
+            public char getD(){
+                return super.character;
+            }
+        }
+    }
+
+    @Test
+    public void run(){
+        Y.Z z = new Y().new Z();
+
+        // so, this way we kind of have two instances of X
+        // look how to get B and D values for X's character field
+
+        assertEquals('A', z.getA());
+        assertEquals('B', z.getB());
+        assertEquals('C', z.getC());
+        assertEquals('D', z.getD());
+
+    }
+
+
+}
