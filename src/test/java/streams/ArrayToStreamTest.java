82e603514dc7503c78610db9b292a2a531f539e4
SVasiliy <vasiliy.sharapov@gmail.com>
2020-02-11 20:42:11 -0600

    none

--- src/test/java/streams/ArrayToStreamTest.java
+++ src/test/java/streams/ArrayToStreamTest.java
@@ -4,9 +4,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.NoSuchElementException;
-import java.util.OptionalDouble;
 import java.util.stream.Stream;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
@@ -30,4 +28,26 @@ public class ArrayToStreamTest {
 
         assertEquals( 0,   Arrays.stream(arr2).average().orElse(0d), 0d);
     }
+
+
+    @Test
+    public void runFlat(){
+
+        Integer[][] arr = { {1, 2, 3}, {1, 2, 3}, {5, 6, 7}, {8, 9, 10, 1, 2} };
+
+        Stream<Integer[]> stream = Arrays.stream(arr);
+
+        assertEquals( 60, stream.flatMap(x -> Arrays.stream(x)).mapToInt( Integer::intValue).sum() );
+
+        assertEquals( 4.5,
+
+                Arrays.stream(arr).flatMap(Arrays::stream)
+                                  .mapToInt( Integer::intValue)
+                                  .average()
+                                  .orElse(0.0)
+                , 0.5);
+    }
+
+
+
 }
