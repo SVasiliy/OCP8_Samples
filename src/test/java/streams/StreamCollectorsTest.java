b4fa3f5c6c306c656e11278c2414659859e31140
SVasiliy <vasiliy.sharapov@gmail.com>
2020-02-13 20:02:58 -0600

    none

--- src/test/java/streams/StreamCollectorsTest.java
+++ src/test/java/streams/StreamCollectorsTest.java
@@ -1,9 +1,12 @@
 package streams;
 
 import beans.BeanFactory;
+import beans.Category;
 import beans.Product;
 import org.junit.Test;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
@@ -26,4 +29,27 @@ public class StreamCollectorsTest {
 
         assertEquals("B1,B2", products);
     }
+
+    @Test
+    public void runGroupBy(){
+
+        List<Product> list = BeanFactory.getProducts();
+
+        Map<Category, List<Product>> collectionA = list.stream()
+                .sorted(Comparator.comparing(Product::getCategory))
+                .collect(Collectors.groupingBy(x -> x.getCategory()));
+
+        assertEquals("{C=[C1, C2, C3], B=[B1, B2], A=[A1]}", collectionA.toString());
+
+        Map<Category, List<Double>> collectionB = list.stream()
+                .sorted(Comparator.comparing(Product::getCategory))
+                .collect(Collectors.groupingBy(Product::getCategory
+                                                    , Collectors.mapping(Product::getPrice, Collectors.toList())
+                                                ));
+
+        assertEquals("{C=[70.0, 60.0, 50.0], B=[90.0, 80.0], A=[100.0]}", collectionB.toString());
+    }
+
+
+
 }
